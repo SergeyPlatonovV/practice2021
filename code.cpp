ef5f5bd8127993f7bb2ba8eb944b4c53f1503626
SergeyPlatonovV <86660798+SergeyPlatonovV@users.noreply.github.com>
2021-06-29 13:54:09 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,6 +1,1 @@
-dajgfdrsdgasojdgfasdgjas
-g[ajgarjogljag;la,g';aerk,gaelkgr,aeglk.,aegrl.kae,rg;lk.ae,g;lkaerg';alekg,pserlkgmrsthjknemlrg,e
-'GFaw'gklwthikjmg'lafG
-:AWf
-er;lgk'esoh;jlms['g;law
-F'wpglkelhjrtkln
+Worker111
